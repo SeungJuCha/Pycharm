0239b68cbb07bd45800ebba7c0f019a20696bd7c
SeungJuCha <113984487+SeungJuCha@users.noreply.github.com>
2022-09-20 20:18:06 +0900

    Delete 2022.09.Class Moel생성.py

--- 2022.09.Class Moel생성.py
+++
@@ -1,9 +0,0 @@
-from SubClassModel import MyModel
-import tensorflow as tf
-
-mymodel = MyModel()
-
-mymodel._name = 'subclass_model'
-
-mymodel(tf.keras.layers.Input(shape = (28,28)))
-mymodel.summary()
